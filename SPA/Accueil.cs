2a3dd6b419dd80c8e25f262454f57b3981e829eb
E <e>
2021-05-08 15:00:08 +0200

    Listing des enquêtes a l’accueil

--- SPA/Accueil.cs
+++ SPA/Accueil.cs
@@ -20,6 +20,21 @@ namespace SPA.Models
         private void Accueil_Load(object sender, EventArgs e)
         {
             labelTitre.Text = "Bienvenue " + utilisateur.Prenom +" " + utilisateur.Nom;
+            List<Enquete> list_enquetes = Enquete.GetEnquetes();
+            if(list_enquetes.Count > 0)
+            {
+                foreach (Enquete enquete in list_enquetes)
+                {
+                    ListViewItem item = new ListViewItem(enquete.Id);
+                    item.SubItems.Add(enquete.Titulaire_enquete.Nom + " " + enquete.Titulaire_enquete.Prenom);
+                    item.SubItems.Add(enquete.Delegue_enqueteur + " " + enquete.Delegue_enqueteur.Prenom);
+                    item.SubItems.Add(enquete.Infracteur.Nom + " " + enquete.Infracteur.Prenom);
+                    item.SubItems.Add(enquete.Plaignant.Nom + " " + enquete.Plaignant.Prenom);
+                    item.SubItems.Add(enquete.Etat.ToString());
+                    item.SubItems.Add(enquete.Avis);
+                    listViewEnquetes.Items.Add(item);
+                }
+            }
         }
 
         private void buttonCreateEnquete_Click(object sender, EventArgs e)
